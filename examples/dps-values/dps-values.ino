b3981b102fcc9dbfa9b3a09afcd56481ff0af03e
Ewan-Dev <ewanmccairn2010@gmail.com>
2025-05-04 23:46:18 +0100

    feat: create new example code file for dps values
    - added gyroscope raw values to dps function in example file

---
+++ examples/dps-values/dps-values.ino
@@ -0,0 +1,26 @@
+#include <mpu6050.h>
+#include <Wire.h>
+
+#define MPU_ADDRESS 0x68 //  mpu6050 address is 0x69 if AD0 pin is powered -  otherwise it's 0x68
+
+float rawGX, rawGY, rawGZ; // initialise raw gyroscope variables
+float dpsGX, dpsGY, dpsGZ; // initialise dps gyroscope variables
+
+void setup(){
+    Serial.begin(115200); // begin serial communication at 115200 baud
+    wakeSensor(MPU_ADDRESS); // wakes sensor from sleep mode
+}
+
+void loop(){
+    readGyroData(MPU_ADDRESS, rawGX, rawGY, rawGZ); // pass MPU6050 address and gyroscope values are written to 3 provided variables
+    rawGyroToDPS(rawGX, rawGY, rawGZ, dpsGX, dpsGY, dpsGZ); // provide the 3 raw gyroscope values and returns them in their dps (degrees per second) values
+    Serial.print("gX:");
+    Serial.print(dpsGX);
+    Serial.print("/");
+    Serial.print("gY:");
+    Serial.print(dpsGY);
+    Serial.print("/");
+    Serial.print("gZ:");
+    Serial.println(dpsGZ);
+    delay(250); // reads at 4Hz
+}
